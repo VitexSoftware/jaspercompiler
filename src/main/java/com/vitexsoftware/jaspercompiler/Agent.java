9581d759bcc2b3a7c74c6ffd097ded3ce85efde7
CyberVitexus <info@vitexsoftware.cz>
2023-09-21 10:27:34 +0200

    try to run with default java

--- src/main/java/com/vitexsoftware/jaspercompiler/Agent.java
+++ src/main/java/com/vitexsoftware/jaspercompiler/Agent.java
@@ -1,14 +1,21 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package com.vitexsoftware.jaspercompiler;
-
+/** Represents an employee.
+ * @author Vítězslav Dvořák
+ * @author vitexsoftware.com
+ * @version 1.0
+ * @since 1.0
+*/
 import java.io.File;
 import java.lang.instrument.Instrumentation;
 import java.util.jar.JarFile;
 
+/**
+ * This class is used to add a jar file to the classpath of the running JVM.
+ * It is used by the JasperReports compiler to add the JasperReports jar file to
+ * the classpath of the JVM that is running the compiler.
+ * This is necessary because the JasperReports compiler uses reflection to
+ * instantiate classes from the JasperReports jar file.
+ */
 public class Agent {
 
     private static Instrumentation inst = null;
